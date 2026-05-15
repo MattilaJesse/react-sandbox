15b80c8e6ea7696267a197ccaab0d0be41009fe5
Jesse Mattila <e2402265@edu.vamk.fi>
2026-05-15 16:03:39 +0300

    lisätty yksinkertainen headeri

--- Harjottelua/src/App.tsx
+++ Harjottelua/src/App.tsx
@@ -1,12 +1,14 @@
 import ProductCatalog from './components/ProductCatalog';
-// import TeamCard from './components/TeamCard'; // Jos haluat pitää vanhat tallessa
+// import TeamCard from './components/TeamCard';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 py-10">
-      {/* Voit jättää TeamCardit tähän jos haluat, 
-          mutta uusi tehtävä vaatii ProductCatalogin: */}
-      <ProductCatalog />
+    <div className="min-h-screen bg-gray-100 py-10 flex flex-col items-center justify-center">
+      {/* Laitetaan tämä tehtävänannon mukainen otsikko näkyviin */}
+      <h1 className="text-3xl font-bold text-gray-800">My Tested App</h1>
+      
+      {/* Kommentoidaan ProductCatalog hetkeksi pois, jotta sivu on mahdollisimman simppeli Verceliä varten */}
+      {/* <ProductCatalog /> */}
     </div>
   );
 }
